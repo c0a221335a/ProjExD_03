15f535a7c7a66df4e549d32dc1b1539b01567d3e
C0A22133 <c0a221335a@edu.teu.ac.jp>
2023-12-05 14:24:57 +0900

    練習1:beamの衝突判定

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -127,6 +127,7 @@ class Beam:
         self.img = pg.image.load(f"{MAIN_DIR}/fig/beam.png")
         self.rct = self.img.get_rect()
         self.rct.center = bird.rct.center
+        self.rct.centerx = bird.rct.centerx+bird.rct.width/2
         self.vx, self.vy = +5, 0
 
     def update(self, screen: pg.Surface):
@@ -153,17 +154,23 @@ def main():
         
         screen.blit(bg_img, [0, 0])
         
-        if bird.rct.colliderect(bomb.rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
+        if beam is not None and bomb is not None:
+            if beam.rct.colliderect(bomb.rct):
+                beam = None
+                bomb = None
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        bomb.update(screen)
-        if beam is not None:
+        if bomb is not None:
+            bomb.update(screen)
+        if beam is not None:  #beamが存在するとき
             beam.update(screen)
 
         pg.display.update()
